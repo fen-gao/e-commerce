8a67d6a6ee416ee9b048503c70a75cb0fa9ac77c
fen-gao <fernando.fnn97@gmail.com>
2024-01-29 09:56:50 -0300

    feat: changed main component and added the provider of application

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
 import "./index.css";
+import { QueryClientProvider } from "react-query";
+import { queryClient } from "./utils/query-client.ts";
+import { GlobalContextProvider } from "./context/global/index.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { DrawerContextProvider } from "./context/drawer/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <DrawerContextProvider>
+          <GlobalContextProvider>
+            <App />
+          </GlobalContextProvider>
+        </DrawerContextProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
