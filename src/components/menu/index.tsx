cd66d9d1c76415b31117844b722d5f7986004771
fen-gao <fernando.fnn97@gmail.com>
2024-01-29 13:32:11 -0300

    fix: added break in menu layout

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -6,7 +6,7 @@ export const MenuList = () => {
 
   return (
     <ul className="container w-full h-full flex flex-row gap-6 px-4 sm:px-6 md:px-10 lg:px-20 mx-auto flex-wrap justify-between">
-      <div className="flex flex-row gap-4">
+      <div className="flex flex-row gap-4 flex-wrap">
         {menu.map((item, index) => (
           <li key={index} className=" text-white hover:text-gray-300">
             <Link to={item.route} className="flex flex-row items-center gap-2">
